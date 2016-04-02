3dd8257de7da46d9c9ae497a52aec39f39730048
SeyDutch <ehfransen@live.nl>
2016-04-02 02:25:35 +0200

    GildeTheme Carousel
    With recipe

---
+++ src/Orchard.Web/Themes/GildeTheme/ResourceManifest.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.UI.Resources;
+
+namespace AirbrushTheme
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+            manifest.DefineStyle("Gildestyle").SetUrl("Gildestyle.min.css");
+            manifest.DefineScript("Vendelscript").SetUrl("Vendelscript.js").SetDependencies("jQuery", "Bootstrap");
+        }
+    }
+}
